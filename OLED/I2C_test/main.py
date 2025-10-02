3b0e2820506707a8d75e14e5840640dd3960c3f6
Hagen Patzke <hagen.patzke@hu.nl>
2025-10-02 10:27:50 +0200

    move I2C scan to OLED, add comments

--- OLED/I2C_test/main.py
+++ OLED/I2C_test/main.py
@@ -1,3 +1,4 @@
+# I2C_test: scan for devices on the I2C bus
 """
 Wil je testen of je je I2C apparaten goed hebt aangesloten? Dan kun je deze code gebruiken.
 Het is een I2C scanner die alle aangesloten I2C apparaten detecteert en hun adres toont.
@@ -7,8 +8,10 @@ Je kunt deze code uitvoeren op een Raspberry Pi Pico W met MicroPython.
 # I2C Scanner MicroPython
 from machine import Pin, SoftI2C
 
-# You can choose any other combination of I2C pins
+# With SoftI2C you can choose any combination of pins
 i2c = SoftI2C(scl=Pin(9), sda=Pin(8))
+# Alternative if you need pull-up resistors on the I2C data and clock line
+# i2c = SoftI2C(scl=Pin(9, Pin.PULL_UP),sda=Pin(8, Pin.PULL_UP))
 
 print('I2C SCANNER')
 devices = i2c.scan()
