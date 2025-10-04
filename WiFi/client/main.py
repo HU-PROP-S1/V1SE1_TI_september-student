b748521396c101a80b6d19f76858b23d1451dab0
harm.snippe <harm.snippe@hu.nl>
2025-10-04 20:21:55 +0200

    Made a simplified point to point WiFi example

--- WiFi/client/main.py
+++ WiFi/client/main.py
@@ -1,106 +1,27 @@
-# client software for point to point wifi
-# from https://forum.core-electronics.com.au/t/pi-pico-w-to-pico-w-simple-communications/16188/15
-import network
-import socket
-import time
+# Client software for point-to-point WiFi on Pico W
+# Simplified and inspired by this example: https://forum.core-electronics.com.au/t/pi-pico-w-to-pico-w-simple-communications/16188/15
+import network, socket, time
 from machine import Pin
-SW_PIN = 15
 
-host  =  '192.168.2.2'
-serverAP =  '192.168.2.1'
-gatewayAP = '192.168.2.1'
-port = 5263
+BTN_PIN = 15
+btn = Pin(BTN_PIN, Pin.IN, Pin.PULL_UP)
 
-Onboard_LED = Pin("LED", Pin.OUT, value=0)
-SW = Pin(SW_PIN, Pin.IN, Pin.PULL_UP)
+sta = network.WLAN(network.STA_IF)
+sta.active(True)
+sta.connect("PICO_AP", "pico1234")
+while not sta.isconnected():
+    time.sleep(0.1)
 
-ssid = 'Demo1234'
-pw = '12345678' # 8 or more characters
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
+server_ip = "192.168.4.1"  # standaard IP van Pico AP
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-def check_SSID():
-    print('Checking for Network ...',ssid)
-    networks = wlan.scan()
-    found = False
-    for w in networks:
-        if w[0].decode() == ssid:
-            found = True
-    return found
+last = btn.value()
+led_state = False
 
-def connect_Fail(s):
-    Error = 'Connect Fail: '
-    if s == 0:
-        Error += 'Link Down'
-    elif s == 1:
-        Error += 'Link Join'
-    elif s == 2:
-        Error += 'Link NoIP'
-    elif s == -1:
-        Error += 'Link Fail'
-    elif s == -2:
-        Error += 'NoNet'
-    elif s == -3:
-        Error += 'BadAuth'
-    else:
-        Error += 'Unknown'
-    print(Error)
-    return
-
-def connect_to_Network():
-    try:
-        if check_SSID():
-            print('Connecting to Network ...')
-            wlan.active(True)
-            wlan.config(pm = 0xa11140)     # Disable power-save mode
-            wlan.connect(ssid, pw)
-            while (wlan.isconnected() == False): time.sleep(1)
-            status = wlan.status()
-            if status == 3:
-                # set IP address to the network we want, otherwise it uses 192.168.4.XXX
-                wlan.ifconfig((host, '255.255.255.0', gatewayAP, '8.8.8.8'))
-                status = wlan.ifconfig()
-                print('ip = ' + status[0])
-                return 1
-            else:
-                connect_Fail(status)
-                print('Cannot connect ... ')
-                return 0
-        else:
-            print('Network NOT found ... ')
-            return 0
-    except Exception as e:
-        print('Error occurred : {}'.format(e))
-        return 0
-
-# Main
-try:
-    if connect_to_Network() == 1:
-        print('Ready')
-        while True:
-            if SW.value() == 0:
-                s = socket.socket()
-                print('Connecting to {}:{}'.format(serverAP,port))
-                s.connect((serverAP, port))
-                data = 'Toggle'
-                s.send(data)
-                count = 0
-                Exit = False
-                while not Exit:
-                    msg = s.recv(512)
-                    if msg.find(b'Success') == -1:
-                        count += 1
-                        if count > 5: Exit = True
-                    else:
-                        print('Success')
-                        Exit = True
-                s.close()
-            time.sleep(0.1)
-            
-except Exception as e:
-    print('Error occurred : {}'.format(e))
-wlan.disconnect()
-time.sleep(0.1)
-wlan.active(False)
-time.sleep(0.1)
-print('Disconnect from Network.')
+while True:
+    v = btn.value()
+    if v != last and v == 0:           # falling edge = knop ingedrukt
+        led_state = not led_state
+        s.sendto(b"ON" if led_state else b"OFF", (server_ip, 9999))
+    last = v
+    time.sleep(0.02)  # debounce
--- WiFi/server/main.py
+++ WiFi/server/main.py
@@ -1,66 +1,21 @@
-# Acces point software for wifi point to point
-# from https://forum.core-electronics.com.au/t/pi-pico-w-to-pico-w-simple-communications/16188/15
-import network
-import socket
-import time
-import uasyncio as asyncio
+# Server software for point-to-point WiFi on Pico W
+# Simplified and inspired by this example: https://forum.core-electronics.com.au/t/pi-pico-w-to-pico-w-simple-communications/16188/15
+import network, socket, time
 from machine import Pin
 
+led = Pin("LED", Pin.OUT)
 
-server = '192.168.2.1'
-gateway = '192.168.2.1'
-port = 5263
-ssid = 'Demo1234'
-pw = '12345678' # minimal 8 characters
-
-Onboard_LED = Pin("LED", Pin.OUT, value=0)
-
-async def SensorData(reader, writer):
-    
-    print("Client connected")
-    SW_State = await reader.read(512)     # returns all charaters including b''
-    SW_State = str(SW_State)              # convert bytes object to str object
-    SW_State = SW_State[2:]               # strip leading b'
-    SW_State = SW_State[:-1]              # strip trailing '
-    
-    if SW_State == 'Toggle':
-        Onboard_LED.toggle()
-
-    writer.write(b'Success')          # send msg back 
-    await writer.drain()
-    await writer.wait_closed()
-    print("Client disconnected")
-
-# main
-async def main():
-# start server monitor task
-    asyncio.create_task(asyncio.start_server(SensorData, "0.0.0.0", port))
-    print('Server listening on {}:{}'.format(server,port))
-
-# Main loop
-    while True:
-        # do something
-        await asyncio.sleep(5)           # need to wait some time to allow server task to function
-        
-try:
-    print('Setting up AP ...')
-    time.sleep(1)
-    wlan = network.WLAN(network.AP_IF)
-    wlan.config(essid=ssid, password=pw)
-    wlan.active(True)
-    time.sleep(1)
-    wlan.ifconfig((server, '255.255.255.0', gateway, '8.8.8.8'))
+ap = network.WLAN(network.AP_IF)
+ap.config(essid="PICO_AP", password="pico1234")
+ap.active(True)
+while not ap.active():
     time.sleep(0.1)
-    time.sleep(2)
 
-    asyncio.run(main())
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("0.0.0.0", 9999))
 
-except Exception as e:
-    print('Error occurred : {}'.format(e))
-finally:
-    asyncio.new_event_loop()
-    wlanAP.disconnect()
-    time.sleep(0.1)
-    wlanAP.active(False)
-    time.sleep(0.1)
-    Onboard_LED.off()
+while True:
+    msg, addr = s.recvfrom(32)
+    m = msg.decode().strip().upper()
+    if m == "ON":  led.value(1)
+    if m == "OFF": led.value(0)
